cf82dd866a9a147ea497deeef66d71d7cc68bc28
Alexander Mescheryakov <sibsfinx@ya.ru>
2018-06-06 20:28:18 +0300

    upd copy

--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
 'use strict';
 
-import jQuery from 'jquery';
-import _ from 'lodash';
-window._ = _;
-window.jQuery = jQuery;
-window.$ = jQuery;
-//import 'bootstrap-sass';
-//require('./favicon.ico'); // Tell webpack to load favicon.ico
 import './styles/styles.sass';
 
-$(() => {
-});
+(function() {
+  const times = new Array();
+  let fps;
+  let avgFps;
+  let fpsSum = 0;
+  let avgCounter = 0;
+  let checkFps = true;
 
+  function refreshLoop() {
+    if (!checkFps) return false;
+    window.requestAnimationFrame(() => {
+      const now = performance.now();
+      while (times.length > 0 && times[0] <= now - 1000) {
+        times.shift();
+      }
+      times.push(now);
+      fps = times.length;
+      avgCounter++;
+      fpsSum = fpsSum + fps;
+      avgFps = fpsSum/avgCounter;
+      console.log(avgFps);
+      refreshLoop();
+    });
+  }
+
+  refreshLoop();
+  setTimeout(() => {
+    checkFps = false;
+  }, 3000);
+
+})();
